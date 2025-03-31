987ed8560bc35a8a45aa377f2378db9f953b961c
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2025-03-31 17:56:57 +0530

    added Comments for TypeScript basics

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-appointment-list',
   imports: [],
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.css'
+  styleUrl: './appointment-list.component.css',
 })
 export class AppointmentListComponent {
 
+  /*
+  //Functions 
+  private log(text: string): void {}
+
+  sum(a: number, b: number) {
+    return a + b;
+  }
+
+  f() {
+    this.log('a')
+    this.log('b')
+  }
+
+  // Primitive Types
+  x: number = 1;
+  y: string = "";
+  z: boolean = true;
+
+  //Object
+  app: AppComponent = new AppComponent();
+
+  //Array Type
+  ax: number[] = [0,0];
+  by: string[] = ["", ""];
+  cz: boolean[] = [true, false];
+
+  */
 }
